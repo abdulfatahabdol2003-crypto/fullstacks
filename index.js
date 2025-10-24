5c66de9bbfed92cf51bfa9a6fe63d98e6d956c39
Abdol2025 <abdulfatahabdol2003@gmail.com>
2025-10-24 14:21:44 -0700

    dfd

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000
   }
